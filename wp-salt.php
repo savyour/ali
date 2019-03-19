bb4372fdf82c5c46f9bca787d40f0e2c8c05ad63
savyour <muhammadali28dec@gmail.com>
2019-03-19 19:35:56 +0000

    Change titles and styling on homepage

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'C2hS8uIs7vzXBJ8YSJmREUSigmANibvxWmutAdc1YUzQq4xERKfpJw8D4h4HW30D');
+define('SECURE_AUTH_KEY',  'CytN4xxhB4zrr2LS0tPmIa7xKfeSfoaSyKh70y3DXVowJ4KHB38ho2AIRiURgz9R');
+define('LOGGED_IN_KEY',    'ItmNJm0Yrm31DjvPNDIAmr9qN914GQ1zLxjG9s0eRLzdPverfRRT1j8teR8SVHBS');
+define('NONCE_KEY',        'HRzt2Id3KARMmVJQp689uMcddMpDVvnprc9ApvnGLvQXbpCPWeXYoAf43BdfwJGI');
+define('AUTH_SALT',        'dPa0magL3wIA3buDpEC7dxqRetR0JewcEdt5qT7gzQ2R0cj8wFXgSSLY56iWBcUv');
+define('SECURE_AUTH_SALT', '7p0XuLDpmHNmJw6AbS5dUCcWdAeLUjfDzHLzwwNyvAbJe7IbY5xNtmGDPup8WsAa');
+define('LOGGED_IN_SALT',   'd0YT6Wsb0RRpxa0e87vtjnxQnLr0fPoDz6sHvnIDfCWdMwzd3QIs9heVFjiCPqgs');
+define('NONCE_SALT',       '8SD1vrBucdsD9FzbsFQafqycMY8S6sRMnFrA7bBtCFJi3fbW4y4KMoFXGARz7n9m');
